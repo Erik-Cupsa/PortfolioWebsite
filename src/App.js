d04ce2a1d226e1c0127acda733d672d6c4306b24
Erik-Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2023-07-31 00:01:20 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
+import React, { useEffect } from 'react';
 import './App.scss';
-import { Route, Routes } from 'react-router-dom'
-import Layout from "./components/Layout"
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import Work from './components/Work';
-import Tech from "./components/About/Tech"
-import Dev from "./components/About/Dev"
+import Tech from './components/About/Tech';
+import Dev from './components/About/Dev';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Erik\'s Portfolio';
+  }, []);
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element = {<Home />}/>
-        <Route path = "about" element = {<About />}/>
-        <Route path = "about/tech" element = {<Tech />}/>
-        <Route path = "about/dev" element = {<Dev />}/>
-        <Route path = "contact" element = {<Contact />}/>
-        <Route path = "portfolio" element = {<Portfolio />}/>
-        <Route path = "work" element = {<Work />}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="about/tech" element={<Tech />} />
+          <Route path="about/dev" element={<Dev />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="work" element={<Work />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
