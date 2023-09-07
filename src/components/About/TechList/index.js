73c8cada92ad7744f44e45313907df88167fc2ac
Erik-Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2023-09-07 17:48:07 -0400

    Add files via upload

--- src/components/About/TechList/index.js
+++ src/components/About/TechList/index.js
@@ -4,12 +4,13 @@ import "./index.scss"
 
 const TechList = () => {
   const [techs] = useState([
-    { name: "React", progress: 100, className: "react-progress", years: 2 },
-    { name: "Spring Boot", progress: 50, className: "springboot-progress", years: 1 },
+    { name: "ReactJS", progress: 100, className: "react-progress", years: 2 },
     { name: "Amazon Web Services (AWS)", progress: 50, className: "aws-progress", years: 1 },
+    { name: "PostgreSQL", progress: 50, className: "postgre-progress", years: 1 },
+    { name: "Spring Boot", progress: 50, className: "springboot-progress", years: 1 },
+    { name: "NodeJS", progress: 50, className: "node-progress", years: 1 },
     { name: "Microsoft Azure DevOps", progress: 50, className: "azure-progress", years: 1 },
     { name: "Docker", progress: 50, className: "docker-progress", years: 1 },
-    { name: "PostgreSQL", progress: 50, className: "postgre-progress", years: 1 },
   ]);
 
   return (
--- src/components/Work/Timeline/index.js
+++ src/components/Work/Timeline/index.js
@@ -5,21 +5,21 @@ const Timeline = () => {
   const [events, setEvents] = useState([
     {
       date: 'May-September 2023',
-      title: 'TRC Companies, Inc.',
+      title: 'Tech Company',
       description: [
-        'Designed <b>30+ AWS Lambda Functions in AWS</b>, increasing the processing speed by <b>35% </b>and reusability by <b>50%</b> through leveraging <b>Lambda layers.</b>',
-        'Implemented <b>15+</b> serverless workflows using <b>AWS Step Functions</b>, seamlessly integrating <b>REST APIs </b>and rigorously tested using <b>Postman</b> to validate functionality and reliability.',
-        'Crafted a <b>Spring Boot</b> application with a <b>Drools</b> logic engine, containerized it using <b>Docker</b>, and seamlessly deployed it on <b>AWS</b> using <b>EC2</b> and <b>Elastic Beanstalk</b> for enhanced scalability and reliability.',
+        'Designed <b>30+ AWS Lambda Functions in AWS</b>, strategically leveraging <b>Lambda layers</b> to increase processing speed by <b>35% </b>and re-usability by <b>52%</b>, resulting in cost savings of <b>$1,000</b> per large scale deployment',
+        'Implemented <b>15+</b> serverless workflows using <b>AWS Step Functions</b>, seamlessly integrating <b>REST APIs </b>and rigorously tested with <b>Postman</b>, resulting in a <b>65%</b> reduction in execution time and enhanced operational efficiency',
+        'Crafted a <b>Spring Boot</b> application with a <b>Drools</b> logic engine, containerized it using <b>Docker</b>, and seamlessly deployed it on <b>AWS</b> using <b>EC2</b>, strategically allocating cloud resources to ensure enhanced scalability and reliability',
       ],
       expanded: false,
     },
     {
       date: 'May-September 2022',
-      title: 'Government of Canada',
+      title: 'Tech Company',
       description: [
-        'Automated documentation by creating a custom <b>Python pipeline </b> to convert PDF and word files to <b>Markdown</b> syntax, saving <b>95%</b> of wiki writing time.',
-        'Collaborated with other coworkers using Python to extract <b>100+</b> codes from a <b>datalake</b> and placing in a <b>pipeline</b> for further extraction',
-        'Centralized the code base for <b>15+</b> software developers on <b>Azure</b>, using <b>SQL</b> to manage databases',
+        'Automated documentation on <b>Azure</b> by constructing a custom <b>Python pipeline</b> to convert PDF and word files to Markdown syntax, saving <b>95%</b> of wiki writing time',
+        'Engineered <b>Python</b> code to efficiently extract and process <b>100+</b> codes from a <b>data lake</b>, seamlessly integrating them into a <b>pipeline</b> for subsequent extraction and analysis, streamlining data workflows and enhancing data utilization',
+        'Centralized the code base for <b>15+</b> software developers on <b>Azure</b>, using <b>SQL</b> to manage databases, resulting in streamlined collaboration, efficient version control, and improved coordination',
       ],
       expanded: false,
     },
